19000a6d32f218cfb285130c1608266b9abf16ca
tranlam <hayashibutler@gmail.com>
2022-10-14 22:09:57 +0700

    update appget

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
-exports.PORT = 5000
+exports.PORT = process.env.PORT || 5000
 exports.ALLOW_URLS = 'http://localhost:4200'
 exports.FETCH_DATA_URL = 'https://api.accesstrade.vn/v1/datafeeds'
 exports.MONGODB = 'mongodb+srv://admin:hayashi0811@databasewebsosanh.nmtynw2.mongodb.net/database'
